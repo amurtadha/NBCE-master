1e7d63066f5f488e22a4b3c4b48bcf907069f937
Murtadha Ahmed <38778295+amurtadha@users.noreply.github.com>
2024-06-21 22:32:25 +0800

    Update eval_text_class.sh

--- scripts/eval_text_class.sh
+++ scripts/eval_text_class.sh
@@ -4,7 +4,7 @@ cd ../
 
 for d in   sst2 sst5 cr cb subj trec rte dbpedia clinic150 banking77 nlu nluscenario trecfine
 do
-  HF_DATASETS_OFFLINE=1 CUDA_VISIBLE_DEVICES=$1  python main.py \
+ CUDA_VISIBLE_DEVICES=$1  python main.py \
   --dataset $d \
   --model $2 \
   --n-windows 3 \
